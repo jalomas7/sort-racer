597dbc2a57dfedffb58aec951f2dd1de08cf25ff
Jacob Massengill <jacob.massengill@asurion.com>
2020-05-20 10:34:23 -0500

    adds a nicer win state

--- src/components/GameWon.tsx
+++ src/components/GameWon.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {useBallContext} from '../providers';
+import {Modal} from 'semantic-ui-react';
 
 const GameWon = () => {
     const {gameWon} = useBallContext();
-    return <div>game won?: {gameWon.toString()}</div>
+    return (
+      <Modal open={gameWon}>
+        <Modal.Content>YOU WIN!!!</Modal.Content>
+      </Modal>
+    );
 };
 
 export default GameWon;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BallProvider} from './providers';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
