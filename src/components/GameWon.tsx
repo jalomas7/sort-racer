75130e24dfd1eb0e58bb41afed395cac7131d778
Jacob Massengill <jacob.massengill@asurion.com>
2020-05-20 11:08:27 -0500

    nicer win state

--- src/components/GameWon.tsx
+++ src/components/GameWon.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import {useBallContext} from '../providers';
-import {Modal} from 'semantic-ui-react';
-import styled from '@emotion/styled';
+import React, { useState, useEffect } from "react";
+import { useBallContext } from "../providers";
+import { Modal } from "semantic-ui-react";
+import {getRandomHexColor, isDark} from '../utils';
+import styled from "@emotion/styled";
 
-const GameWonResetButton = styled.button`
-    outline: none;
+const GameWonContainer = styled.div<{background: string}>`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: ${({background}) => background};
+    color: ${({background}) => isDark(background) ? 'white' : 'black'};
+    padding: 10em;
 `;
 
+const GameWonResetButton = styled.button`
+  outline: none;
+`;
+
+const GameWonHeader = styled.h1``;
+
 const GameWon = () => {
-    const {gameWon, resetGame} = useBallContext();
-    return (
-      <Modal open={gameWon}>
-        <Modal.Content>YOU WIN!!!</Modal.Content>
-        <GameWonResetButton onClick={resetGame}>Play Again</GameWonResetButton>
-      </Modal>
-    );
+  const { gameWon, resetGame } = useBallContext();
+  const [color, setColor] = useState('transparent');
+
+  useEffect(() => {
+    setColor(getRandomHexColor());
+  }, [])
+
+  return (
+    <Modal open={gameWon} basic small>
+      <Modal.Content>
+        <GameWonContainer background={color}>
+          <GameWonHeader>YOU WIN!!!</GameWonHeader>
+          <GameWonResetButton onClick={resetGame}>
+            Play Again
+          </GameWonResetButton>
+        </GameWonContainer>
+      </Modal.Content>
+    </Modal>
+  );
 };
 
 export default GameWon;
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -6,3 +6,21 @@ export const getRandomHexColor = (brightness=0): string => {
     }
     return color;
 };
+
+export const isDark = (color: string): boolean => {
+    if (!color) {
+        return false;
+    }
+
+    const pureColor = color.slice(1, color.length);
+
+    if (pureColor.length !== 6) {
+        return false;
+    }
+
+    const R = parseInt(pureColor.slice(0, 2), 16);
+    const G = parseInt(pureColor.slice(2, 4), 16);
+    const B = parseInt(pureColor.slice(4, 6), 16);
+
+    return R + G + B < 382;
+};
