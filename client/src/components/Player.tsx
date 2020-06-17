63b87393c9f9d6a537cdf0a0de45b17ef533f6ca
Jacob Massengill <jacob.massengill@asurion.com>
2020-06-17 15:52:04 -0500

    prettier fixes

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,8 +1,8 @@
-import React, { useState, FunctionComponent, useEffect } from "react";
-import styled from "@emotion/styled";
-import BallStack from "./BallStack";
-import Ball from "./Ball";
-import { useBallContext, useGameContext } from "../providers";
+import React, {useState, FunctionComponent, useEffect} from 'react';
+import styled from '@emotion/styled';
+import BallStack from './BallStack';
+import Ball from './Ball';
+import {useBallContext, useGameContext} from '../providers';
 
 const PlayerContainer = styled.div`
     display: flex;
@@ -20,49 +20,49 @@ export type PlayerProps = {
     playerId: string;
 };
 
-const Player: FunctionComponent<PlayerProps> = ({ playerId }) => {
-  const { activeBall } = useBallContext();
-  const { playerStacks } = useGameContext();
-  const [xPos, setXPos] = useState<number>(0);
-  const [yPos, setYPos] = useState<number>(0);
-  const [ws, setWs] = useState<WebSocket>();
+const Player: FunctionComponent<PlayerProps> = ({playerId}) => {
+    const {activeBall} = useBallContext();
+    const {playerStacks} = useGameContext();
+    const [xPos, setXPos] = useState<number>(0);
+    const [yPos, setYPos] = useState<number>(0);
+    const [ws, setWs] = useState<WebSocket>();
 
-  useEffect(() => {
-    const thisWs = new WebSocket("ws://localhost:8080");
-    thisWs.addEventListener("open", () => {
-      setWs(thisWs);
-    });
-    thisWs.addEventListener("close", () => {
-      thisWs.close();
-    });
-    thisWs.addEventListener('message', ev => {
-      console.log(ev);
-    });
+    useEffect(() => {
+        const thisWs = new WebSocket('ws://localhost:8080');
+        thisWs.addEventListener('open', () => {
+            setWs(thisWs);
+        });
+        thisWs.addEventListener('close', () => {
+            thisWs.close();
+        });
+        thisWs.addEventListener('message', (ev) => {
+            console.log(ev);
+        });
 
-    return () => {
-      if (ws) {
-        ws.close();
-        setWs(undefined);
-      }
+        return () => {
+            if (ws) {
+                ws.close();
+                setWs(undefined);
+            }
+        };
+    }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+    const updateActiveBallPos = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setXPos(e.clientX);
+        setYPos(e.clientY);
+        if (ws && ws.OPEN) {
+            ws.send(
+                JSON.stringify({
+                    [playerId]: {xPos: e.clientX, yPos: e.clientY},
+                }),
+            );
+        }
     };
-  }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
-  const updateActiveBallPos = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setXPos(e.clientX);
-    setYPos(e.clientY);
-    if (ws && ws.OPEN) {
-      ws.send(
-        JSON.stringify({
-          [playerId]: { xPos: e.clientX, yPos: e.clientY },
-        })
-      );
+    if (!playerStacks[playerId]) {
+        return <React.Fragment />;
     }
-  };
-
-  if (!playerStacks[playerId]) {
-    return <React.Fragment />;
-  }
 
     return (
         <PlayerContainer onMouseMoveCapture={updateActiveBallPos} id={playerId}>
--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,24 @@
-import WebSocket from "ws";
+import WebSocket from 'ws';
 
-const wss = new WebSocket.Server({ port: 8080, host: "localhost"});
+const wss = new WebSocket.Server({port: 8080, host: 'localhost'});
 
-console.log("server running at localhost:8080");
+console.log('server running at localhost:8080');
 
-wss.on("connection", function connection(ws) {
-  console.log("connection recieved");
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-  });
+wss.on('connection', function connection(ws) {
+    console.log('connection recieved');
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
 
-  ws.on('close', () => {
-      console.log('connection closed');
-  })
+    ws.on('close', () => {
+        console.log('connection closed');
+    });
 
-  ws.send("something");
+    ws.send('something');
 });
 
-wss.on("message", function incoming(data) {
-  console.log(data);
+wss.on('message', function incoming(data) {
+    console.log(data);
 });
 
 wss.on('close', () => {
