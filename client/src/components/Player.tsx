b773323daaef3827784c123793359433ad49d8f2
Jacob Massengill <jacob.massengill@asurion.com>
2020-06-17 11:39:43 -0500

    simple echo connection setup

--- client/src/components/Player.tsx
+++ client/src/components/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent } from "react";
+import React, { useState, FunctionComponent, useEffect } from "react";
 import styled from "@emotion/styled";
 import BallStack from "./BallStack";
 import Ball from "./Ball";
@@ -25,15 +25,33 @@ const Player: FunctionComponent<PlayerProps> = ({ playerId }) => {
   const { playerStacks } = useGameContext();
   const [xPos, setXPos] = useState<number>(0);
   const [yPos, setYPos] = useState<number>(0);
+  const [ws, setWs] = useState<WebSocket>();
+
+  useEffect(() => {
+    setWs(new WebSocket("ws://localhost:8080"));
+
+    return () => {
+      if (ws) {
+        ws.close();
+      }
+    };
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
   const updateActiveBallPos = (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     setXPos(e.clientX);
     setYPos(e.clientY);
+    if (ws) {
+      ws.send(
+        JSON.stringify({
+          [playerId]: { xPos: e.clientX, yPos: e.clientY },
+        })
+      );
+    }
   };
 
-  if(!playerStacks[playerId]) {
-    return <React.Fragment />
+  if (!playerStacks[playerId]) {
+    return <React.Fragment />;
   }
 
   return (
--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,26 @@
-import WebSocket from 'ws';
- 
-const wss = new WebSocket.Server({ port: 8080, host: 'localhost' });
+import WebSocket from "ws";
 
-console.log('server running at localhost:8080');
+const wss = new WebSocket.Server({ port: 8080, host: "localhost"});
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+console.log("server running at localhost:8080");
+
+wss.on("connection", function connection(ws) {
+  console.log("connection recieved");
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
   });
- 
-  ws.send('something');
+
+  ws.on('close', () => {
+      console.log('connection closed');
+  })
+
+  ws.send("something");
 });
 
 wss.on("message", function incoming(data) {
   console.log(data);
+});
+
+wss.on('close', () => {
+    console.log('server closed');
 });
